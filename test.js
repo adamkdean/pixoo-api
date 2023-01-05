acd1b556c7b9f039990916036ce0bd9a5b1bc67c
Adam K Dean <adamkdean@googlemail.com>
2023-01-05 21:36:15 +0000

    add color lib + pixel draw

--- test.js
+++ test.js
@@ -3,6 +3,7 @@
 // license that can be found in the LICENSE file.
 
 import { PixooAPI } from './lib/api.js'
+import { Color } from './lib/color.js'
 
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
@@ -20,11 +21,38 @@ async function main() {
   // const currentChannel = await api.getCurrentChannel()
   // console.log('currentChannel', currentChannel)
 
+  // Random fill colors
+  // while (true) {
+  //   const color = [Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255)]
+  //   console.log(api.counter, 'setting color', color)
+  //   api.fill(color[0], color[1], color[2])
+  //   await api.push()
+  // }
+
+  // Move pixel randomly
+  let x = 32
+  let y = 32
   while (true) {
-    const color = [Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255)]
-    console.log(api.counter, 'setting color', color)
-    api.fill(color[0], color[1], color[2])
+    const direction = Math.floor(Math.random() * 4)
+    switch (direction) {
+      case 0:
+        x++
+        break
+      case 1:
+        x--
+        break
+      case 2:
+        y++
+        break
+      case 3:
+        y--
+        break
+    }
+
+    api.fill(Color.Black)
+    api.drawPixel(x, y, Color.White)
     await api.push()
+    console.log(api.pushCount, 'pushed', api.pushAvgElapsed.toFixed(2), 'ms (avg)')
   }
 }
 
