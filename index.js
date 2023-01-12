5853033ca75e08c2d5319d938e70afa3f05dc50d
Adam K Dean <adamkdean@googlemail.com>
2023-01-12 15:53:48 +0000

    bump version to 0.2.2

---
+++ index.js
@@ -0,0 +1,7 @@
+// Copyright (C) 2022 Adam K Dean <adamkdean@googlemail.com>
+// Use of this source code is governed by the GPL-3.0
+// license that can be found in the LICENSE file.
+
+export { PixooAPI } from './lib/api.js'
+export { Color } from './lib/color.js'
+export { Font } from './lib/fonts.js'
